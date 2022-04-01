909a419f539b42382afc5a4d3d9623c43081e848
Tauras Petrauskas <petrauskas.post@gmail.com>
2022-04-01 14:35:55 +0300

    Initial setup done. Added dependencies for Spring Boot (Web, Data JPA, Security, Dev Tools), MySQL, Thymeleaf, Log4j. Created MySQL DB "bootcamp". Initial configuration done in "application.properties".

---
+++ src/main/java/com/bootcamp/bootcampmanager/BootcampManagerApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.bootcampmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootcampManagerApplication.class, args);
+    }
+
+}
