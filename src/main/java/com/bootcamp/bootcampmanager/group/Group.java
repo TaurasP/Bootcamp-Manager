0b50fd2843d4f3c6e52b97ec24a605a7f7ffbb28
Ansis Maleckis <maleckisansis@gmail.com>
2022-04-04 13:29:14 +0300

    removed getters and setters

--- src/main/java/com/bootcamp/bootcampmanager/group/Group.java
+++ src/main/java/com/bootcamp/bootcampmanager/group/Group.java
@@ -2,6 +2,9 @@ package com.bootcamp.bootcampmanager.group;
 
 import com.bootcamp.bootcampmanager.project.Project;
 import com.bootcamp.bootcampmanager.student.Student;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -9,6 +12,9 @@ import java.util.List;
 
 @Entity
 @Table(name = "Groups")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Group {
 
     @Id
@@ -28,36 +34,5 @@ public class Group {
             inverseJoinColumns =
                     { @JoinColumn(name = "project_id", referencedColumnName = "id") })
     private Project project;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Student> getStudents() {
-        return students;
-    }
-
-    public void setStudents(List<Student> students) {
-        this.students = students;
-    }
-
-    public Project getProject() {
-        return project;
-    }
-
-    public void setProject(Project project) {
-        this.project = project;
-    }
+    
 }
