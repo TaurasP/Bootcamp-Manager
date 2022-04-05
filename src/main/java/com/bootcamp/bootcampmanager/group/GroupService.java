29c5f5c4eaa26d9a220f06bc5423322039a19025
Ansis Maleckis <maleckisansis@gmail.com>
2022-04-05 12:49:53 +0300

    starting

--- src/main/java/com/bootcamp/bootcampmanager/group/GroupService.java
+++ src/main/java/com/bootcamp/bootcampmanager/group/GroupService.java
@@ -6,9 +6,4 @@ import java.util.List;
 
 public interface GroupService {
     List<Group> getAllGroups();
-
-    void saveGroup(Group group);
-
-    Group getGroupById(long id);
-    void deleteGroupById(long id);
 }
--- src/main/java/com/bootcamp/bootcampmanager/group/GroupServiceImp.java
+++ src/main/java/com/bootcamp/bootcampmanager/group/GroupServiceImp.java
@@ -19,20 +19,4 @@ public class GroupServiceImp implements GroupService{
     public List<Group> getAllGroups(){
         return groupRepository.findAll();
     }
-
-    @Override
-    public void saveGroup(Group group) {
-
-        groupRepository.save(group);
-    }
-
-    @Override
-    public Group getGroupById(long id) {
-        return groupRepository.getById(id);
-    }
-
-    @Override
-    public void deleteGroupById(long id) {
-        groupRepository.deleteById(id);
-    }
 }
