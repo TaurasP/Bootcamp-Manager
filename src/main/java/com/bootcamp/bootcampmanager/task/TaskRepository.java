2a933c828709a1a9d82084764d2a846a91d0427c
anete bremane <anetebremane@gmail.com>
2022-04-07 14:22:37 +0300

    Added tasks to course view page

--- src/main/java/com/bootcamp/bootcampmanager/task/TaskRepository.java
+++ src/main/java/com/bootcamp/bootcampmanager/task/TaskRepository.java
@@ -3,7 +3,10 @@ package com.bootcamp.bootcampmanager.task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByCourse_IdEquals(long id);
 
 }
--- src/main/java/com/bootcamp/bootcampmanager/task/TaskServiceImp.java
+++ src/main/java/com/bootcamp/bootcampmanager/task/TaskServiceImp.java
@@ -3,9 +3,16 @@ package com.bootcamp.bootcampmanager.task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TaskServiceImp implements TaskService{
 
     @Autowired
     public TaskRepository taskRepository;
+
+    @Override
+    public List<Task> getTasksByCourse(long courseId) {
+        return taskRepository.findByCourse_IdEquals(courseId);
+    }
 }
