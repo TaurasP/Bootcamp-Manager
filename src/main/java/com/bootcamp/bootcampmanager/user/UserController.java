5bc4f1d50b0e50cb941c3b2809fae00069f4c442
Ansis Maleckis <maleckisansis@gmail.com>
2022-04-12 13:24:48 +0300

    Email verification 1.

--- src/main/java/com/bootcamp/bootcampmanager/user/UserController.java
+++ src/main/java/com/bootcamp/bootcampmanager/user/UserController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -62,10 +63,24 @@ public class UserController {
     }
 
     @PostMapping("/save-user")
-    public String saveUser(@ModelAttribute("user") User user) {
+    public String saveUser(@ModelAttribute("user") User user, Model model) {
         user.setEnabled(true);
+        model.addAttribute("email", user.getEmail());
         if(user.getRoles() == null)
             return "redirect:/users";
+        for(User i : userService.getAllUsers())
+            if(i.getEmail().equals(user.getEmail()))
+                return "redirect:/repeating-emails";
+        for(Student i : studentService.getAllStudents())
+            if(i.getEmail().equals(user.getEmail()))
+                return "redirect:/repeating-emails";
+        for(Admin i : adminService.getAllAdmins())
+            if(i.getEmail().equals(user.getEmail()))
+                return "redirect:/repeating-emails";
+        for(Lecturer i : lecturerService.getAllLecturers())
+            if(i.getEmail().equals(user.getEmail()))
+                return "redirect:/repeating-emails";
+
         if(user.getRoles().equals("ROLE_ADMIN")){
             passwordGeneratorService.generateRandomPassword(user);
             Admin admin = new Admin(user);
@@ -85,6 +100,12 @@ public class UserController {
         MailThread mailThread = new MailThread(mailService, user);
         mailThread.start();
         return "redirect:/users";
+    }
+
+    @GetMapping("/repeating-emails")
+    public String repeatingEmail(Model model) {
+        model.addAttribute("email", "phony");
+        return "repeating-emails";
     }
 
     @GetMapping("/update-user/{id}")
